c110116582de86b9740f654296094134d611a35b
samyak9430-web <samyak.9430@gmail.com>
2025-08-28 12:12:41 +0530

    Add files via upload

---
+++ AREA_PERIMETER.c
@@ -0,0 +1,15 @@
+//Azhar, 13th sep 2024
+//wap to calculate area and perimeter of rectangle
+#include <stdio.h>
+void main()
+{
+int length, width, area, perimeter;
+printf("Enter the length");
+scanf("%d",&lemgth);
+printf("Enter the width");
+scanf("%d",& width);
+area=length*width;
+printf("area=%d\n",area);
+perimeter=2*(length+width);
+printf("perimeter=%d\n",perimeter);
+}
